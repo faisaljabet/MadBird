17affda1fb2e71af5526f23144441657410e2c96
FaisalNirjhor <faisaljabet@gmail.com>
2023-06-04 12:15:43 +0600

    Main Project

---
+++ Assets/Bird.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+    private bool _birdWasLaunched;
+
+    [SerializeField] private float _launchPower = 250;
+    private float _timeSittingAround;
+
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+
+
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 100 ||
+            transform.position.y < -100 ||
+            transform.position.x > 100 ||
+            transform.position.x < -100 ||
+            _timeSittingAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.green;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+     
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 diractionToInitialPosition = _initialPosition - transform .position;
+
+        GetComponent<Rigidbody2D>().AddForce(diractionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
---
+++ Assets/Rat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rat : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FlyCat flyCat = collision.collider.GetComponent<FlyCat>(); 
+        if(flyCat != null)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Rat rat = collision.collider.GetComponent<Rat>();
+        if(rat != null)
+        {
+            return;
+        }
+
+        if(collision.contacts[0].normal.y < -0.5 || collision.contacts[0].normal.x < -0.5)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
